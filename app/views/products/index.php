bbc0e260c89d22393d6f4a1629fead4415cbe14b
Diahdwi09 <diahdwisulintangg@gmail.com>
2023-12-13 16:05:36 +0700

    rubah url

---
+++ app/views/products/index.php
@@ -0,0 +1,29 @@
+<h2>Products</h2>
+
+<a href="<?php echo URL; ?>/products/input" class="btn">Input User</a>
+
+<table>
+      <tr>
+            <th>ID</th>
+            <th>JENIS</th>
+            <th>UMUR</th>
+            <th>HARGA</th>
+            <th>KETERANGAN HEWAN</th>
+      </tr>
+
+      <?php $no = 1;
+      foreach ($data['rows'] as $row) { ?>
+            <tr>
+                  <td><?php echo $no; ?></td>
+                  <td><?php echo $row['product_id']; ?></td>
+                  <td><?php echo $row['product_jenis']; ?></td>
+                  <td><?php echo $row['product_umur']; ?></td>
+                  <td><?php echo $row['product_harga']; ?></td>
+                  <td><?php echo $row['product_keterangan_hewan']; ?></td>
+                  <td><a href="<?php echo URL; ?>/products/edit/<?php echo $row['product_id']; ?>" class="btn">Edit</a></td>
+                  <td><a href="<?php echo URL; ?>/products/delete/<?php echo $row['product_id']; ?>" class="btn" onclick="return confirm('Are you sure?')">Delete</a></td>
+            </tr>
+      <?php $no++;
+      } ?>
+
+</table>
---
+++ app/views/products/input.php
@@ -0,0 +1,30 @@
+<h2>Input Product</h2>
+
+<form action="<?php echo URL; ?>/products/save" method="post">
+    <table>
+        <tr>
+            <td>ID</td>
+            <td><input type="id" name="id" required></td>
+        </tr>
+        <tr>
+            <td>JENIS</td>
+            <td><input type="text" name="password" required></td>
+        </tr>
+        <tr>
+            <td>UMUR</td>
+            <td><input type="text" name="umur" required></td>
+        </tr>
+        <tr>
+            <td>HARGA</td>
+            <td><input type="text" name="harga" required></td>
+        </tr>
+        <tr>
+            <td>KETERANGAN HEWAN</td>
+            <td><input type="text" name="keterangan_hewan" required></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" name="btn_save" value="SAVE"></td>
+        </tr>
+    </table>
+</form>
